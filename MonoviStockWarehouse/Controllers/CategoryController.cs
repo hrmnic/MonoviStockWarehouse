cd7c201b47b6d262100f57a40ee61e66a8e6b8cc
Özge Yurdusev <53175095+hrmnic@users.noreply.github.com>
2022-05-19 00:41:17 +0300

    Category Index changes

--- MonoviStockWarehouse/Controllers/CategoryController.cs
+++ MonoviStockWarehouse/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MonoviStockWarehouse.Models.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,25 @@ namespace MonoviStockWarehouse.Controllers
 {
     public class CategoryController : Controller
     {
+        Context c = new Context();
+
         public ActionResult Index()
+        {
+            var values = c.Categories.ToList();
+            
+            return View(values);
+        }
+        [HttpGet]
+        public ActionResult CategoryAdd()
         {
             return View();
         }
-
+        [HttpPost]
+        public ActionResult CategoryAdd(Category cat)
+        {
+            c.Categories.Add(cat);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
